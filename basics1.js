d3707ae25821e0a9d645008d9f244fd5345764a0
Ajay Krishnan <ajaykrishnan315@gmail.com>
2026-08-08 19:03:12 +0530

    Initial commit

---
+++ basics1.js
@@ -0,0 +1,31 @@
+console.log("Hello World")
+
+ let a=4   // let is used to declare a variable with block scope
+ console.log(a)
+ console.log(typeof a)  // typeof is used to check the type of variable
+
+ var b=4.5 // var is used to declare a variable with function scope
+ console.log(b)
+ console.log(typeof b)  
+
+ let c="Hello"  
+ console.log(typeof c)
+
+ const required= true // const is used to declare a variable with block scope and cannot be reassigned
+ console.log(typeof required)
+
+ //null is used to declare a variable with no value
+ //undefined is used to declare a variable with no value assigned to it   
+
+  //let c= a+b ( it will give an error because c is already declared)
+ c= a+b 
+ console.log(c)
+
+ console.log(!required)  // ! is used to negate a boolean value
+
+// line comments
+
+/*
+sfsfsf
+ffsfsf
+*/
